64db38077d8761103676f1441a8d494b3707ecc1
nathanle89 <nathanle89@gmail.com>
2015-06-11 08:44:42 -0700

    Gas robo

--- scripts/phantom/robo.js
+++ scripts/phantom/robo.js
@@ -1,13 +1,47 @@
+var fs = require('fs');
+var path = 'gas_price_output.csv';
+var system = require('system');
+
+
 var page = require('webpage').create();
-page.open('http://www.amsoil.com/mygarage/vehiclelookuppage.aspx', function() {
-    page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-        var result = page.evaluate(function() {
-            $("#ctl00_bph1_wucGarageSelector1_txtYear").val("2012");
-            $("#ctl00$bph1$wucGarageSelector1$btnBuild").click();
-            return $("#ctl00_bph1_wucGarageSelector1_tvVehicles").children("table").length;
+
+page.onConsoleMessage = function(msg) {
+    system.stderr.writeLine(msg);
+};
+
+page.open('http://www.gasbuddy.com', function() {
+    page.injectJs("jquery-1.11.3.min.js");
+
+    page.evaluate(function() {
+        $("#search-text").val("94401");
+        $("#search-btn").click();
+    });
+
+    setTimeout(function() {
+        var resultString = page.evaluate(function() {
+            var gasStations = $('#prices-table-cheapest').children('tr');
+            var result = '';
+            for (var i = 0; i < gasStations.length; i++) {
+                var $gasData = $(gasStations[i]).children();
+                var price = $($gasData[0]).find('.gb-price')[0].innerText;
+                var gasStationName = $($($gasData[1]).children()[0]).find('a strong')[0].innerText;
+                var address = $($gasData[1]).children()[1].innerText;
+                var city = $gasData[2].innerText.trim();
+
+                result = result + (price + ';' + gasStationName + ';' + address + ';' + city) + ':';
+            }
+
+            return result;
         });
 
-        console.log(result);
+        if (resultString.length > 0) {
+            var results = resultString.split(':');
+            for (var i = 0; i < results.length; i++) {
+                fs.write(path, results[i] + '\n', 'a');
+            }
+        }
+
         phantom.exit()
-    });
+    }, 1000)
 });
+
---
+++ scripts/phantom/test.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nguyenle
+ * Date: 6/9/15
+ * Time: 10:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var page = require('webpage').create();
+console.log('The default user agent is ' + page.settings.userAgent);
+page.settings.userAgent = 'SpecialAgent';
+page.open('http://www.httpuseragent.org', function(status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        var ua = page.evaluate(function() {
+            return document.getElementById('myagent').textContent;
+        });
+        console.log(ua);
+    }
+    phantom.exit();
+});
